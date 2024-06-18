fd5f29a94107066c55febf9b2cc9426233746aa1
Youda Zhou <youda.zhou@accenture.com>
2024-06-18 09:23:53 +0200

    Initial Commit

---
+++ Event Countdown/Event_CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  Event_CountdownApp.swift
+//  Event Countdown
+//
+//  Created by Youda Zhou on 18/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Event_CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
